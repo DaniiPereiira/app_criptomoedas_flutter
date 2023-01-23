e0b4642359784ce3d7c62985f60159c0ddbdeb55
Dani Pereira <pdaniele@outlook.com.br>
2023-01-23 19:29:02 -0300

    Atualização no botão favoritos, inclusão do botão para selecionar todas as moedas, melhorias na página favoritos.

--- lib/search/custom_sarche.dart
+++ lib/search/custom_sarche.dart
@@ -1,91 +1,91 @@
-import 'package:app_cripto/Moedas/moeda.dart';
-import 'package:flutter/material.dart';
+// import 'package:app_cripto/Moedas/moeda.dart';
+// import 'package:flutter/material.dart';
 
-class CustomSearchDelegate extends SearchDelegate {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      )
-    ];
-  }
+// class CustomSearchDelegate extends SearchDelegate {
+//   @override
+//   List<Widget> buildActions(BuildContext context) {
+//     return [
+//       IconButton(
+//         onPressed: () {
+//           query = '';
+//         },
+//         icon: const Icon(Icons.clear),
+//       )
+//     ];
+//   }
 
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
+//   @override
+//   Widget buildLeading(BuildContext context) {
+//     return IconButton(
+//       onPressed: () {
+//         close(context, null);
+//       },
+//       icon: const Icon(Icons.arrow_back),
+//     );
+//   }
 
-  @override
-  Widget buildResults(BuildContext context) {
-    if (query.length < 3) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Center(
-            child: Text(
-              "Search term must be longer than two letters.",
-            ),
-          )
-        ],
-      );
-    }
-    using
-    InheritedBlocs.of(context)
-        .searchBloc
-        .searchTerm
-        .add(query);
+//   @override
+//   Widget buildResults(BuildContext context) {
+//     if (query.length < 3) {
+//       return Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: const <Widget>[
+//           Center(
+//             child: Text(
+//               "Search term must be longer than two letters.",
+//             ),
+//           )
+//         ],
+//       );
+//     }
+//     using
+//     InheritedBlocs;.of(context)
+//         .searchBloc
+//         .searchTerm
+//         .add(query);
 
-    return Column(
-      children: <Widget>[
+//     return Column(
+//       children: <Widget>[
        
-        StreamBuilder(
-          stream: InheritedBlocs.of(context).searchBloc.searchResults,
-          builder: (context, AsyncSnapshot<List<Moeda>> snapshot) {
-            if (!snapshot.hasData) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Center(child: CircularProgressIndicator()),
-                ],
-              );
-            } else if (snapshot.data?.length == 0) {
-              return Column(
-                children: const <Widget>[
-                  Text(
-                    "No Results Found.",
-                  ),
-                ],
-              );
-            } else {
-              var results = snapshot.data;
-              return ListView.builder(
-                itemCount: results.length,
-                itemBuilder: (context, index) {
-                  var result = results[index];
-                  return ListTile(
-                    title: Text(result.title),
-                  );
-                },
-              );
-            }
-          },
-        ),
-      ],
-    );
-  }
+//         StreamBuilder(
+//           stream: InheritedBlocs.of(context).searchBloc.searchResults,
+//           builder: (context, AsyncSnapshot<List<Moeda>> snapshot) {
+//             if (!snapshot.hasData) {
+//               return Column(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: const <Widget>[
+//                   Center(child: CircularProgressIndicator()),
+//                 ],
+//               );
+//             } else if (snapshot.data?.length == 0) {
+//               return Column(
+//                 children: const <Widget>[
+//                   Text(
+//                     "No Results Found.",
+//                   ),
+//                 ],
+//               );
+//             } else {
+//               var results = snapshot.data;
+//               return ListView.builder(
+//                 itemCount: results.length,
+//                 itemBuilder: (context, index) {
+//                   var result = results[index];
+//                   return ListTile(
+//                     title: Text(result.title),
+//                   );
+//                 },
+//               );
+//             }
+//           },
+//         ),
+//       ],
+//     );
+//   }
 
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return null;
-  }
-}
+//   @override
+//   Widget buildSuggestions(BuildContext context) {
+//     return null;
+//   }
+// }
